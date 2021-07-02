95a1a115cafe34be092961943f01b777454d89d9
Adam Gunter <agunt002@ucr.edu>
2021-07-01 22:45:33 -0700

    Third commit

---
+++ Lab4_StateMachines/turnin/agunt002_lab4_part3.c
@@ -0,0 +1,97 @@
+/*	Author: lab
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum States {Start, Init, UnlockPound, UnlockY, UnlockYRelease, Locked} state;
+void Tick() {
+	switch(state) {
+		case Start:
+			state = Init;
+			break;
+		case Init:
+			if ((PINA & 0x07) == 0x04) {
+				state = UnlockPound;
+			}
+			else if ((PINA & 0x87) == 0x80) {
+				state = Locked;
+			}
+			else {
+				state = Init;
+			}
+			break;
+		case UnlockPound:
+			if ((PINA & 0x07) == 0x04) {
+                                state = UnlockPound;
+                        }
+                        else {
+                                state = UnlockY;
+                        }
+                        break;
+		case UnlockY:
+			if ((PINA & 0x07) == 0x02) {
+                                state = UnlockYRelease;
+                        }
+			else if (PINA == 0x00) {
+				state = UnlockY;
+			}
+                        else {
+                                state = Init;
+                        }
+                        break;
+		case UnlockYRelease:
+			if ((PINA & 0x07) == 0x02) {
+                                state = UnlockYRelease;
+                        }
+                        else {
+                                state = Init;
+                        }
+                        break;
+		case Locked:
+			if ((PINA & 0x87) == 0x80) {
+                                state = Locked;
+                        }
+                        else {
+                                state = Init;
+                        }
+                        break;
+	}
+	switch(state) {
+		case Start:
+			PORTB = 0x00;
+			break;
+		case Init:
+			break;
+		case UnlockPound:
+			break;
+		case UnlockY:
+			break;
+		case UnlockYRelease:
+			PORTB = 0x01;
+			break;
+		case Locked:
+			PORTB = 0x00;
+			break;
+
+	}
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    /* Insert your solution below */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+    while (1) {
+	Tick();
+    }
+    return 1;
+}
