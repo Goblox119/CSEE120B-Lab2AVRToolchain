f6ac7746aecb11c5cc242abc836a95e68ca95e37
Adam Gunter <agunt002@ucr.edu>
2021-07-01 21:47:25 -0700

    Fifth commit

---
+++ Lab3_BitManipulation/turnin/agunt002_lab3_part5.c
@@ -0,0 +1,44 @@
+/*	Author: lab
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    /* Insert your solution below */
+    DDRB = 0x01; PORTB = 0x00;
+    DDRD = 0x00; PORTD = 0xFF;
+    unsigned char tempB = 0x00;
+    unsigned char tempD = 0x00;
+    unsigned char finalB = 0x00;
+    unsigned short totalWeight = 0x0000;
+
+    while (1) {
+	tempD = PIND;
+	tempB = PINB;
+	finalB = 0x00;
+	totalWeight = (tempD << 1) | (tempB & 0x01);
+	if (totalWeight >= 0x0046) {
+		finalB = 0x02;
+	}
+	else if (totalWeight > 0x0005) {
+		finalB = 0x04;
+	}
+	else {
+		finalB = 0x00;
+	}
+	PORTB = finalB;
+	
+    }
+    return 1;
+}
