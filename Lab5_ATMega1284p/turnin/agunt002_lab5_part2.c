ee16c197aa0dcc7d32c3657df756f4d060f940c9
Adam Gunter <agunt002@ucr.edu>
2021-07-07 21:24:31 -0700

    Second commit

---
+++ Lab5_ATMega1284p/turnin/agunt002_lab5_part2.c
@@ -0,0 +1,112 @@
+/*	Author: lab
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum States {Start, Begin, Init, Reset, Plus, Minus, PlusOn, MinusOn} state;
+void Tick() {
+	switch(state) {
+		case Start:
+			state = Begin;
+			break;
+		case Begin:
+			state = Init;
+			break;
+		case Init:
+			if ((~PINA & 0x03) == 0x03) {
+                                state = Reset;
+                        }
+			else if ((~PINA & 0x01) == 0x01) {
+				state = PlusOn;
+			}
+			else if ((~PINA & 0x02) == 0x02) {
+                                state = MinusOn;
+                        }
+			break;
+		case Reset:
+			if ((~PINA & 0x03) == 0x03) {
+                                state = Reset;
+                        }
+                        else {
+                                state = Init;
+                        }
+                        break;
+		case Plus:
+			if ((~PINA & 0x03) == 0x01) {
+                                state = Plus;
+                        }
+                        else {
+                                state = Init;
+                        }
+                        break;
+		case PlusOn:
+			state = Plus;
+			break;
+			
+		case Minus:
+                        if ((~PINA & 0x03) == 0x02) {
+                                state = Minus;
+                        }
+                        else {
+                                state = Init;
+                        }
+                        break;
+		case MinusOn:
+			state = Minus;
+			break;
+			
+		default:
+			state = Start;
+			break;
+	}
+	switch(state) {
+		case Start:
+			PORTC = 0x07;
+			break;
+		case Begin:
+			PORTC = 0x07;
+			break;
+		case Init:
+			break;
+		case Plus:
+			break;
+		case Minus:
+			break;
+		case PlusOn:
+			if (PORTC < 0x09) {
+                        	PORTC = PORTC + 1;
+			}
+                        break;
+		case MinusOn:
+			if (PORTC > 0x00) { 
+                        	PORTC = PORTC - 1;
+			}
+                        break;
+		case Reset:
+			PORTC = 0x00;
+			break;
+		default:
+			PORTC = 0x07;
+			break;
+	}
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    /* Insert your solution below */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRC = 0xFF; PORTC = 0x00;
+    while (1) {
+	Tick();
+    }
+    return 1;
+}
